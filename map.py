f14b3e8f6065ad44b3edcd6aaf8417bdbd15a7ef
Eric Defore <eric.defore@gmail.com>
2019-08-29 08:47:23 -0400

    Make finding refereces to other Actors/Objects a little easier. I am using this to find Path data for Korok Block/Goals and Flowers

--- map.py
+++ map.py
@@ -25,13 +25,27 @@ for outfilename,folder_suffix in files:
         dropactor_objs = (root.findall('./*/value') +
                           root.findall('./Rails/*/RailPoints/value'))
 
+        index = 0
         for actor in dropactor_objs:
+            HashId = index
+            if 'HashId' in actor.attrib :
+                HashId = actor.attrib["HashId"]
             name = actor.findall('./UnitConfigName')[0].text
             coords = [float(node.text[:-1]) for node in actor.findall('./Translate/value')]
             if len(coords) == 0:
                 continue
             scale = [float(node.text[:-1]) for node in actor.findall('./Scale/value')]
             rotation = [float(node.text[:-1]) for node in actor.findall('./Rotate/value')]
+
+            references = []
+
+            for node in actor.findall('./LinksToObj/value') :
+                if ( node.findall('./DefinitionName')[0].text == 'Reference' or 
+                    node.findall( '.DefinitionName' )[0].text == 'BasicSig' or 
+                    node.findall( '.DefinitionName' )[0].text == 'Create' or 
+                    node.findall( '.DefinitionName' )[0].text == 'BasicSigOnOnly' ) : 
+                    references.append( node.attrib['DestUnitHashId'] )
+
             if len(rotation) == 0:
                 if 'Rotate' in actor.attrib:
                     rotation = [0,float(actor.attrib['Rotate'][:-1]),0]
@@ -71,24 +85,44 @@ for outfilename,folder_suffix in files:
                 name = 'HARD:'+name
                 nice_name = 'HARD:'+nice_name
             if name not in objects:
-                objects[name] = {'display_name':nice_name, 'locations':[]}
-            objects[name]['locations'].append([round(coords[0],2), round(coords[-1],2)])
+                objects[name] = {'display_name':nice_name, 'locations':{}}
+
+            objects[name]['locations'][HashId] = {}
+            
+            objects[name]['locations'][HashId]['coords'] = [round(coords[0],2), round(coords[-1],2)]
+
+            # Another place to store it for us to use later in our own loop
+            objects[name]['locations'][HashId]['name'] = name 
+
+            firstKorokFlower = False
+            if ( name == 'Obj_Plant_Korok_A_01' ) : 
+
+                if ( actor.find( './_Parameters' ) is not None and 'IsNoAppearEffect' in actor.find( './_Parameters' ).attrib ) :
+                    firstKorokFlower = actor.find( './_Parameters' ).attrib["IsNoAppearEffect"]
+            
+                objects[name]['locations'][HashId]['firstKorokFlower'] = firstKorokFlower
+
+            if len( references ) != 0 :
+                objects[name]['locations'][HashId]['references'] = references
+
             #if len(scale):
             #    objects[name]['locations'][-1].append({'width':scale[0],'height':scale[-1],'rotation':rotation[1]})
 
-    for filename in os.listdir('blwp'+folder_suffix):
-        f=open('blwp'+folder_suffix+'/'+filename,'rb')
-        data = f.read()
-        f.close()
-        chunk_objects = prod.parseProd(data)
-        for name in chunk_objects:
-            if name in object_names:
-                nice_name = object_names[name]
-            else:
-                nice_name = name
-            if name not in objects:
-                objects[name] = {'display_name':nice_name, 'locations':[]}
-            objects[name]['locations'] += [(round(x,2),round(z,2)) for x,y,z in chunk_objects[name]]
+            index += 1
+
+    #for filename in os.listdir('blwp'+folder_suffix):
+    #    f=open('blwp'+folder_suffix+'/'+filename,'rb')
+    #    data = f.read()
+    #    f.close()
+    #    chunk_objects = prod.parseProd(data)
+    #    for name in chunk_objects:
+    #        if name in object_names:
+    #            nice_name = object_names[name]
+    #        else:
+    #            nice_name = name
+    #        if name not in objects:
+    #            objects[name] = {'display_name':nice_name, 'locations':{}}
+    #        objects[name]['locations'][0]['coords'] += [(round(x,2),round(z,2)) for x,y,z in chunk_objects[name]]
 
     outfile = open(outfilename,'w')
     outfile.write('var locations = ')
